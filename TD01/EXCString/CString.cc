aee61d36dc0e84632754f2dc7fdf43b2fbf7a98c
Gwennael Cannenpasse <Gwennaelcan@gmail.com>
2020-09-24 13:27:48 +0200

    EXO6 fonctionnel

--- TD01/EXCString/CString.cc
+++ TD01/EXCString/CString.cc
@@ -1,28 +1,75 @@
 #include <iostream>
 #include <cstdlib>
-#include <string.h>
+#include <cstring>
 #include "CString.h"
 using namespace std;
 
 CString::CString() 
 {
-	chaine = new char [1];
-	strcpy(chaine,'');
+	chaine = new char[1];
+	//strcpy(chaine,'/o');
+	nbr_chaine++;
 }
 
 CString::CString(char a) 
 {
-	chaine = new char [1];
-	strcpy(chaine,a);
+	chaine = new char[2];
+	chaine[0] = a;
+	chaine[1] = 0;
+	nbr_chaine++;
 }
 
 CString::CString(char *a) 
 {
 	chaine = new char [strlen(a)+1];
-	strcpy(chaine,a);
+	chaine = a;
+	nbr_chaine++;
 }
 
-CString::getString()
+char *CString::getString()
 {
 	return chaine;
+}
+
+char * CString::plus(char *string)
+{
+	// Ici on definit la taille de "nouvelle" qui sera la taille des deux chaines ajoutées
+	
+	char *nouvelle = new char[strlen(chaine) + 1];
+	nouvelle = chaine;
+	
+	// On fait la même chose sur str , et on concatene les chaines dans str :
+	chaine = new char[strlen(nouvelle) + strlen(chaine) + 1];
+	strcat(chaine, nouvelle);
+	strcat(chaine, string);
+	return chaine;
+}
+
+
+int CString::nbrChaines()
+{
+	return nbr_chaine;
+}
+
+bool CString::plusGrandQue(CString &string){
+	
+	int a = strlen(chaine);
+	int b = strlen(string.getString());
+	
+	if (a > b) return true;
+	else return false;
+	
+}
+
+bool CString::infOuEgale(CString &string){
+	
+	char alphabet[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q','r','s','t','u','v','w','x','y','z'};
+	
+	for(int i(0); i<26; ++i){
+		if ((chaine[i] == alphabet[i]) && (string.getString() [i] == alphabet[i])) continue;
+		else if((chaine[i] == alphabet[i]) && (string.getString() [i] != alphabet[i])) return true;
+		else if((chaine[i] != alphabet[i]) && (string.getString() [i] == alphabet[i])) return false;
+	}
+		
+	return -1;
 }
--- TD01/EXCString/Exo6.cc
+++ TD01/EXCString/Exo6.cc
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <cstdlib>
 #include <string.h>
 #include "CString.h"
 using namespace std;
@@ -10,13 +9,13 @@ int main()
 	s2( 'q' ),
 	s3 ;
 
-	cout << "nbrChaines" << CString::nbrChaines() << endl ;
+	//cout << "nbrChaines" << CString::nbrChaines() << endl ;
 	//afficher le nombre de chaines créées
-	s3 = s1.plus( ‘w’ ) ;
+	s3 = s1.plus( "w" ) ;
 	cout << "s3=" << s3.getString() << endl ;
 	if( s1.plusGrandQue(s2) ) // si s1 > s2 au sens alphabétique
 		cout << "plus grand" << endl ;
 	if( s1.infOuEgale(s2) ) // si s1 <= s2 au sens alphabétique
 		cout << "plus petit" << endl ;
-	s3 = s1.plusGrand( s2 ) ;// retourner s1 si s1>s2, s2 sinon
+	s3 = s1.plusGrandQue( s2 ) ;// retourner s1 si s1>s2, s2 sinon
 } 
