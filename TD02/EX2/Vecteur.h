37620aa8fbf92c1d37f0e3c888f99266472c2e6b
Pierre-Louis <pierre-louis.chardon@ens.uvsq.fr>
2020-10-12 15:36:55 +0200

    faire l'implementation de liste chainée

--- TD02/EX2/Vecteur.h
+++ TD02/EX2/Vecteur.h
@@ -1,11 +1,25 @@
 #ifndef DEF_VECTEUR
 #define DEF_VECTEUR
 
+
+struct Element{
+	Element *suivant;
+	int valeur;
+};
+
+struct Liste{
+	Element *premier;
+};
+
+
+
+
 class Vecteur{
 	// Attributs :
 	private:
 	int *m_set;
-    int m_dim;      //Dimansion
+    int m_dim;      //Dimension
+    // Liste *l;
 	
 	public:
 	// Constructeurs :
@@ -20,12 +34,18 @@ class Vecteur{
 	int getDim() const;
 	int* getTableau() const;
 	void afficherTab() const;
+	// void AJoutElement(int i);
+	// int getElement(int i);
 	
 	/***** SURCHARGE D'OPERATEURS   ********/
 	Vecteur& operator=(const Vecteur& v);
 	int& operator[](int i);
 	friend Vecteur operator+(const Vecteur& v1, const Vecteur& v2);
-	Vecteur& operator+=(const Vecteur& v);
+	Vecteur& operator+=(const Vecteur& v); 
+	// bool PlusGrandQue(Vecteur& s);
+	// bool InfOuEgal(Vecteur& v);
+	// bool operator > (const Vecteur& v1, const Vecteur& v2);
+	// bool operator <= (const Vecteur& v1, const Vecteur& v2);
 	
 };
 
